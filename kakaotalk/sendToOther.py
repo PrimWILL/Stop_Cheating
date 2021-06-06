a2f5b4a7ebe648363ea7e63a8ed9a16cc2910979
donghyun <ilovekdh1208@ajou.ac.kr>
2021-06-06 18:04:27 +0900

    send message to others

---
+++ kakaotalk/sendToOther.py
@@ -0,0 +1,44 @@
+with open(r"C:\Users\user\Desktop\PythonWorkspace\kakao_test\kakao_code.json","r") as fp:
+    tokens = json.load(fp)
+# print(tokens)
+# print(tokens["access_token"])
+
+friend_url = "https://kapi.kakao.com/v1/api/talk/friends"
+
+# GET /v1/api/talk/friends HTTP/1.1
+# Host: kapi.kakao.com
+# Authorization: Bearer {ACCESS_TOKEN}
+
+headers={"Authorization" : "Bearer " + tokens["access_token"]}
+
+result = json.loads(requests.get(friend_url, headers=headers).text)
+
+print(type(result))
+print("=============================================")
+print(result)
+print("=============================================")
+friends_list = result.get("elements")
+print(friends_list)
+# print(type(friends_list))
+print("=============================================")
+print(friends_list[0].get("uuid"))
+friend_id = friends_list[0].get("uuid")
+print(friend_id)
+
+send_url= "https://kapi.kakao.com/v1/api/talk/friends/message/default/send"
+
+data={
+    'receiver_uuids': '["{}"]'.format(friend_id),
+    "template_object": json.dumps({
+        "object_type":"text",
+        "text":"성공입니다!",
+        "link":{
+            "web_url":"www.daum.net",
+            "web_url":"www.naver.com"
+        },
+        "button_title": "바로 확인"
+    })
+}
+
+response = requests.post(send_url, headers=headers, data=data)
+response.status_code
